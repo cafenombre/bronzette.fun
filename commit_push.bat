13befd6b4568554f9817c9e359015f6b72a572ef
cafenombre <tristan.glotin@gmail.com>
2020-10-22 19:14:00 +0200

    init

---
+++ commit_push.bat
@@ -0,0 +1,24 @@
+
+::@echo off
+set httpsgit="https://github.com/cafenombre/bronzette.fun.git"
+
+REM SET UP GIT ENV :
+::git init
+::git remote add origin %httpsgit%
+::git remote -v
+::pause
+
+REM AutoCommit
+set /p cmsg="Commit message: "
+
+::track all files
+git add .                           
+::track deletes 
+git add -u              
+::commit with messages          
+git commit -m "%cmsg%"      
+::push to origin      
+git push origin master              
+
+
+pause
---
+++ pull.bat
@@ -0,0 +1,7 @@
+@echo off
+
+::git fetch origin master
+
+git pull origin master
+
+pause
